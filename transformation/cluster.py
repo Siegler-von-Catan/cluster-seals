06f7df7cd433c421b76c81436f17e680fcf5177d
leogeier <leonard.geier@hotmail.com>
2020-12-27 19:49:30 +0100

    add a few more dimreduction methods

--- transformation/cluster.py
+++ transformation/cluster.py
@@ -3,16 +3,46 @@
 from argparse import ArgumentParser
 import sqlite3
 from pathlib import Path
-from sklearn.decomposition import PCA
+from sklearn.decomposition import PCA, TruncatedSVD
+from sklearn.manifold import TSNE, Isomap
+
+
+def pca(feature_vectors):
+    return PCA(n_components=2).fit_transform(feature_vectors)
+
+
+def tsne(feature_vectors):
+    return TSNE(n_components=2).fit_transform(feature_vectors)
+
+
+def tsvd(feature_vectors):
+    return TruncatedSVD(n_components=2).fit_transform(feature_vectors)
+
+
+def isomap(feature_vectors):
+    return Isomap(n_components=2).fit_transform(feature_vectors)
+
+
+dim_reduction_method = {
+    "pca": pca,
+    "tsne": tsne,
+    "tsvd": tsvd,
+    "isomap": isomap
+}
 
 
 def main():
     parser = ArgumentParser()
     parser.add_argument("db_path", type=Path)
     parser.add_argument("out_path", type=Path)
+    parser.add_argument("--dimred", "-r", default="pca",
+                        choices=dim_reduction_method.keys(),
+                        help="Dimensionality reduction method to use")
     args = parser.parse_args()
 
     conn = sqlite3.connect(args.db_path)
+
+    print("Creating feature vectors...")
 
     all_tags = conn.execute("SELECT name FROM tag").fetchall()
     all_tags = {name[0]: i for i, name in enumerate(all_tags)}
@@ -30,9 +60,11 @@ def main():
     # for thing in tags_by_seal:
     #     print(thing)
 
-    pca = PCA(n_components=2)
-    coords = pca.fit_transform(feature_vectors)
+    print("Reducing dimensions...")
 
+    coords = dim_reduction_method[args.dimred](feature_vectors)
+
+    print("Writing results...")
     with open(args.out_path, "w") as out_file:
         out_file.write("id,x,y\n")
         for coord, tbs in zip(coords, tags_by_seal):
